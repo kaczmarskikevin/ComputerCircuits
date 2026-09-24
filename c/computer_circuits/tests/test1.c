1db4a2ff82db828a14e331077f4242203bd1cc89
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2026-09-23 20:39:19 -0400

    test

--- c/computer_circuits/tests/test1.c
+++ c/computer_circuits/tests/test1.c
@@ -18,19 +18,30 @@ static int tests_failed = 0;
 
 // Connect all wires to all faces.
 int test_copper_wire(void) {
-    copper_wire_t* main_wire;
-    copper_wire_t* north_wire;
-    copper_wire_t* south_wire;
-    copper_wire_t* east_wire;
-    copper_wire_t* west_wire;
-    copper_wire_t* top_wire;
-    copper_wire_t* bottom_wire;
-    return connect_face(main_wire,north_wire,COPPER_NORTH) ||
-           connect_face(main_wire,south_wire,COPPER_SOUTH) ||
-           connect_face(main_wire,east_wire,COPPER_EAST) ||
-           connect_face(main_wire,west_wire,COPPER_WEST) ||
-           connect_face(main_wire,top_wire,COPPER_TOP) ||
-           connect_face(main_wire,bottom_wire,COPPER_BOTTOM);
+    // 1. Initialize/allocate your pointers so they don't cause segfaults
+    copper_wire_t main_wire_obj = {0}; 
+    copper_wire_t north_obj = {0}, south_obj = {0}, east_obj = {0};
+    copper_wire_t west_obj = {0}, top_obj = {0}, bottom_obj = {0};
+
+    copper_wire_t* main_wire   = &main_wire_obj;
+    copper_wire_t* north_wire  = &north_obj;
+    copper_wire_t* south_wire  = &south_obj;
+    copper_wire_t* east_wire   = &east_obj;
+    copper_wire_t* west_wire   = &west_obj;
+    copper_wire_t* top_wire    = &top_obj;
+    copper_wire_t* bottom_wire = &bottom_obj;
+
+    // 2. Use bitwise OR (|) or independent addition to prevent short-circuiting
+    int result = 0;
+
+    result |= connect_face(main_wire, north_wire,  COPPER_NORTH);
+    result |= connect_face(main_wire, south_wire,  COPPER_SOUTH);
+    result |= connect_face(main_wire, east_wire,   COPPER_EAST);
+    result |= connect_face(main_wire, west_wire,   COPPER_WEST);
+    result |= connect_face(main_wire, top_wire,    COPPER_TOP);
+    result |= connect_face(main_wire, bottom_wire, COPPER_BOTTOM);
+
+    return result; 
 }
 
 //Connect the north wire to a face that doesn't exist.
