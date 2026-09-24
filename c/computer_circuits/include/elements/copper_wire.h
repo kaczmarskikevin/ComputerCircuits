5521738668a2e2914b1071df5f8eabfece19315f
Kevin Kaczmarski <kaczmarskikevin@gmail.com>
2026-09-23 20:53:23 -0400

    test

--- c/computer_circuits/include/elements/copper_wire.h
+++ c/computer_circuits/include/elements/copper_wire.h
@@ -8,12 +8,12 @@
 #define COPPER_BOTTOM 5
 
 typedef struct copper_wire_t {
-    struct copper_wire_t* face_north_neighbor;
-    struct copper_wire_t* face_south_neighbor;
-    struct copper_wire_t* face_east_neighbor;
-    struct copper_wire_t* face_west_neighbor;
-    struct copper_wire_t* face_top_neighbor;
-    struct copper_wire_t* face_bottom_neighbor;
+    copper_wire_t* face_north_neighbor;
+    copper_wire_t* face_south_neighbor;
+    copper_wire_t* face_east_neighbor;
+    copper_wire_t* face_west_neighbor;
+    copper_wire_t* face_top_neighbor;
+    copper_wire_t* face_bottom_neighbor;
 
     // We will use positive and negative voltage to determine 
     //  electron flow.
